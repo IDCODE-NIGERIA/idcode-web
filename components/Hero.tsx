1ff5a98048571903277249712ba113471ac03d9f
Nduka Ugochukwu <62814725+thatzerroguy@users.noreply.github.com>
2026-04-20 12:37:49 +0100

    fix: viewport

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export const Hero = () => {
     return (
-        <section className="bg-[#F5F5F5] pt-16 sm:pt-28 lg:pt-16 pb-8 sm:pb-10">
-            <div className="max-w-7xl lg:h-[67vh] mx-auto px-4 sm:px-6 lg:px-16 grid grid-cols-1 lg:grid-cols-2 gap-8 sm:gap-16 lg:gap-26 items-center">
+        <section className="bg-[#F5F5F5] pt-20 sm:pt-28 lg:pt-24 pb-12 sm:pb-16">
+            <div className="max-w-7xl lg:min-h-[67vh] mx-auto px-4 sm:px-6 lg:px-16 grid grid-cols-1 lg:grid-cols-2 gap-8 sm:gap-16 lg:gap-26 items-center">
                 {/* Left Column */}
                 <div className="max-w-xl">
                     <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-onSurface leading-tight">
-                        Verification and Access Control that builds trust in a digital age
+                        Identification, Verification and Access Control that builds trust in a digital age
                     </h1>
                     <p className="mt-4 sm:mt-6 text-base sm:text-lg text-onSurfaceMuted leading-relaxed">
                         IDCODE Nigeria is a tech-driven company specializing in verification and access control solutions. Through our integration with NIMC, CAC, NAFDAC and other identity services, We help businesses stay ahead of growing fraud risk and impersonation threats.
@@ -24,7 +24,7 @@ export const Hero = () => {
                             alt="Hero Image" 
                             width={1000} 
                             height={1000} 
-                            className="w-full h-auto max-w-md sm:max-w-lg lg:max-w-full object-cover" 
+                            className="w-full h-automax-w-md sm:max-w-lg lg:max-w-xl object-cover" 
                             loading="eager"
                         />
                     </div>
